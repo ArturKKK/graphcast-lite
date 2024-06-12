89b5648788625cb7dc71b7faad06b89f039ae637
Adit Whorra <aditwhorra@gmail.com>
2024-06-12 20:27:55 +0200

    Fixed dataset loading

--- src/constants.py
+++ src/constants.py
@@ -6,5 +6,5 @@ class FileNames:
     EXPERIMENT_CONFIG = "config.json"
     TRAIN_X = "X_train.pt"
     TRAIN_Y = "y_train.pt"
-    TEST_X = "X_train.pt"
-    TEST_Y = "y_train.pt"
+    TEST_X = "X_test.pt"
+    TEST_Y = "y_test.pt"
