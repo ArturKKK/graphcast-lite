1ab136568b28c8453a3781257f406175990ba3e2
Adit Whorra <aditwhorra@gmail.com>
2024-06-13 18:23:21 +0200

    Fixed indices

--- src/models.py
+++ src/models.py
@@ -25,6 +25,12 @@ from src.mesh.create_mesh import (
     get_hierarchy_of_triangular_meshes_for_sphere,
 )
 
+from src.utils import (
+    get_bipartite_graph_spatial_features,
+    get_mesh_lat_long,
+    GraphStructure,
+)
+
 
 class MLP(nn.Module):
     def __init__(self, mlp_config: MLPBlock, input_dim):
@@ -132,19 +138,18 @@ class WeatherPrediction(nn.Module):
     """This is our main weather prediction model. Similar to GraphCast, this model will
       operate on three graphs -
 
-      * Grid2Mesh graph: This graph contains all the nodes. This graph is strictly
-      bipartite with edges going from grid nodes to the mesh nodes.The encoder model will
-      operate on this graph. The output of this stage will be a latent representation for
-      the mesh nodes.
+    * Encoding graph: This graph contains all the nodes. This graph is strictly
+    bipartite with edges going from grid nodes to the mesh nodes.
+    The output of this stage will be a latent representation for
+    the mesh nodes.
 
-    * Mesh graph: This graph contains only the mesh nodes. The process model will
-      operate on this graph. It will update the latent state of the mesh nodes.
+    * Processing Graph: This graph contains only the mesh nodes.
+    It will update the latent state of the mesh nodes.
 
-    * Mesh2Grid graph: This graph contains all nodes. This graph is strictly
+    * Decoding graph: This graph contains all nodes. This graph is strictly
       bipartite with edges going from mesh nodes to grid nodes such that each grid
       nodes is connected to 3 nodes of the mesh triangular face that contains
-      the grid points. The decode model will operate on this graph. It will
-      process the updated latent state of the mesh nodes, and the latent state
+      the grid points. It will process the updated latent state of the mesh nodes, and the latent state
       of the grid nodes, to produce the final output for the grid nodes.
     """
 
@@ -161,34 +166,39 @@ class WeatherPrediction(nn.Module):
         self.num_features = data_config.num_features
         self.total_feature_size = self.timesteps * self.num_features
 
-        _meshes = get_hierarchy_of_triangular_meshes_for_sphere(
-            splits=graph_config.mesh_size
-        )
-        finest_mesh = _meshes[-1]
+        self._init_grid_properties(grid_lat=cordinates[0], grid_lon=cordinates[1])
+        self._init_mesh_properties(graph_config)
 
-        self.num_grid_nodes = len(cordinates[0]) * len(cordinates[1])
-        self.num_mesh_nodes = finest_mesh.vertices.shape[0]
-
-        self.encoding_graph = create_encoding_graph(
-            cordinates=cordinates,
-            mesh=finest_mesh,
-            graph_building_config=graph_config,
+        self.encoding_graph, self.init_grid_features, self.init_mesh_features = (
+            create_encoding_graph(
+                grid_node_lats=self._grid_lat,
+                grid_node_longs=self._grid_lon,
+                mesh_node_lats=self._mesh_nodes_lat,
+                mesh_node_longs=self._mesh_nodes_lon,
+                mesh=self._finest_mesh,
+                graph_building_config=graph_config,
+                num_grid_nodes=self._num_grid_nodes,
+            )
         )
+
+        # The shape of the initial static features that are added to each node
+        self._init_feature_size = self.init_grid_features.shape[1]
 
         self.processing_graph = create_processing_graph(
-            meshes=_meshes, mesh_levels=graph_config.mesh_levels
+            meshes=self._meshes, mesh_levels=graph_config.mesh_levels
         )
 
         self.decoding_graph = create_decoding_graph(
             cordinates=cordinates,
-            mesh=finest_mesh,
+            mesh=self._finest_mesh,
             graph_building_config=graph_config,
+            num_grid_nodes=self._num_grid_nodes,
         )
 
         self.encoder = Model(
-            model_config=pipeline_config.encoder, input_dim=self.total_feature_size
+            model_config=pipeline_config.encoder,
+            input_dim=self.total_feature_size + self._init_feature_size,
         )
-        
 
         self.processor = Model(
             model_config=pipeline_config.processor, input_dim=self.encoder.output_dim
@@ -197,6 +207,56 @@ class WeatherPrediction(nn.Module):
         self.decoder = Model(
             model_config=pipeline_config.decoder, input_dim=self.processor.output_dim
         )
+
+    def _init_grid_properties(self, grid_lat: np.ndarray, grid_lon: np.ndarray):
+        self._grid_lat = grid_lat.astype(np.float32)
+        self._grid_lon = grid_lon.astype(np.float32)
+        self._num_grid_nodes = grid_lat.shape[0] * grid_lon.shape[0]
+
+    def _init_mesh_properties(self, graph_config: GraphBuildingConfig):
+        self._meshes = get_hierarchy_of_triangular_meshes_for_sphere(
+            splits=graph_config.mesh_size
+        )
+        self._finest_mesh = self._meshes[-1]
+        self._num_mesh_nodes = len(self._finest_mesh.vertices)
+
+        self._mesh_nodes_lat, self._mesh_nodes_lon = get_mesh_lat_long(
+            finest_mesh=self._finest_mesh
+        )
+
+        self._mesh_nodes_lat, self._mesh_nodes_lon = self._mesh_nodes_lat.astype(
+            np.float32
+        ), self._mesh_nodes_lon.astype(np.float32)
+
+    def _preprocess_input(self, grid_node_features: torch.Tensor):
+        batch_size, _, _ = grid_node_features.shape
+
+        # Repeat initial features across all batches
+        broadcasted_init_grid_features = self.init_grid_features.broadcast_to(
+            (batch_size, self._num_grid_nodes, self._init_feature_size)
+        )
+        broadcasted_mesh_grid_features = self.init_mesh_features.broadcast_to(
+            (batch_size, self._num_mesh_nodes, self._init_feature_size)
+        )
+
+        # Concatenate the initial grid node features with the incoming input
+        updated_grid_node_features = torch.cat(
+            (grid_node_features, broadcasted_init_grid_features), dim=-1
+        )
+
+        # Initialise the mesh node features to 0s and append the initial mesh features
+        mesh_node_features = torch.zeros(
+            (batch_size, self._num_mesh_nodes, self.total_feature_size)
+        )
+
+        updated_mesh_node_features = torch.cat(
+            (mesh_node_features, broadcasted_mesh_grid_features), dim=-1
+        )
+
+        # Concatenate them into one single tensor so that they can be passed through graph layers
+        X = torch.cat((updated_grid_node_features, updated_mesh_node_features), dim=1)
+
+        return X
 
     def forward(self, X: torch.Tensor):
         """The forward method takes the features of the grid nodes and passes them through the three graphs defined above.
@@ -208,15 +268,30 @@ class WeatherPrediction(nn.Module):
           The input data of the shape [batch, num_grid_nodes, num_features].
         """
 
-        mesh_node_features = self.encoder(X=X, edge_index=self.encoding_graph)
+        X = self._preprocess_input(grid_node_features=X)
 
+        encoded_features = self.encoder(X=X, edge_index=self.encoding_graph)
+
+        grid_node_features = encoded_features[:, : self._num_grid_nodes, :]
+        mesh_node_features = encoded_features[:, self._num_grid_nodes :, :]
+
+        # Processing the mesh node features
         processed_mesh_node_features = self.processor(
             X=mesh_node_features, edge_index=self.processing_graph
         )
 
+        # Concatenating the grid feature again with the processed mesh features
+        processed_features = torch.cat(
+            (grid_node_features, processed_mesh_node_features), dim=1
+        )
+
         decoded_grid_node_features = self.decoder(
-            X=processed_mesh_node_features,
+            X=processed_features,
             edge_index=self.decoding_graph,
         )
-        
+
+        decoded_grid_node_features = decoded_grid_node_features[
+            :, : self._num_grid_nodes, :
+        ]
+
         return decoded_grid_node_features
